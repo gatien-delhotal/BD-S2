9301b8cf4d242dcff68a883bdddac0fb1f34559c
Gatien Delhotal <gatien.delhotal@etu.umontpellier.fr>
2020-02-28 16:00:52 +0100

    FIn de séance

---
+++ TP5.sql
@@ -0,0 +1,74 @@
+--R5A
+SELECT COUNT(*), categorieService
+FROM Services
+GROUP BY categorieService;
+
+--R5B
+SELECT villeClient
+FROM Clients
+GROUP BY villeClient
+HAVING COUNT(*)>=3;
+
+--R50
+SELECT COUNT(*), nomClient
+FROM Locations l
+JOIN Clients c ON c.idClient = l.idClient
+GROUP BY nomClient, c.idClient
+ORDER BY COUNT(*) DESC;
+
+--R51
+SELECT nomCamping, AVG(salaireEmploye)
+FROM Employes e
+JOIN Campings c ON e.idCamping = c.idCamping
+GROUP BY nomCamping;
+
+--R53
+SELECT nomCamping
+FROM Campings c
+JOIN Employes e ON e.idCamping = c.idCamping
+GROUP BY c.idCamping, nomCamping
+HAVING COUNT(*)  > 3;
+
+--R54 PAS OK DU TOUT
+SELECT nomCamping
+FROM Campings c
+JOIN Bungalows b ON b.idCamping = c.idCamping
+GROUP BY nomCamping
+HAVING MIN(superficieBungalow) < 65
+ORDER BY COUNT(*);
+
+--R55
+SELECT nomCamping
+FROM Campings c 
+JOIN Employes e ON c.idCamping =  e.idCamping
+GROUP BY nomCamping
+HAVING MIN(salaireEmploye) >= 1000;
+
+--R56
+SELECT nomBungalow
+FROM Bungalows b
+JOIN Proposer s ON b.idBungalow = s.idBungalow
+GROUP BY nomBungalow
+HAVING COUNT(*) = (SELECT COUNT(*)
+	FROM Bungalows b
+	JOIN Proposer s ON b.idBungalow = s.idBungalow
+	WHERE nomBungalow = 'Le Royal');
+
+--R57
+SELECT nomBungalow, COUNT(idService)
+FROM Bungalows b
+LEFT JOIN Proposer p 
+ON b.idBungalow = p.idBungalow
+GROUP BY nomBungalow, b.idBungalow
+ORDER BY COUNT(idService) DESC;
+
+--R58
+SELECT nomBungalow, nomCamping
+FROM Bungalows b
+JOIN Campings c ON b.idCamping = c.idCamping
+WHERE idBungalow IN(
+	SELECT idBungalow
+	FROM Locations
+	GROUP BY idBungalow, dateFin, dateDebut
+	HAVING dateFin > '01-06-2017'
+	AND dateDebut < '30-06-2017');
