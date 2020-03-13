4c3d7b9b57b971c1ad08539d13667803add6a06e
Gatien Delhotal <gatien.delhotal@etu.umontpellier.fr>
2020-03-13 15:53:07 +0100

    La 89 reste à faire

--- TP5.sql
+++ TP5.sql
@@ -117,3 +117,104 @@ WHERE NOT EXISTS (
 	SELECT idService
 	FROM Proposer p
 	WHERE p.idBungalow = b.idBungalow);
+
+
+--R81
+
+SELECT c.idClient, nomClient, prenomClient
+FROM Clients c
+WHERE idClient NOT IN(
+	SELECT c.idClient
+	FROM Clients c
+	JOIN Locations l ON c.idClient = l.idClient
+	JOIN Bungalows b ON b.idBungalow = l.idBungalow
+	JOIN Campings ca ON ca.idCamping = b.idCamping
+	WHERE villeCamping = 'Palavas'
+	)
+ORDER BY nomClient, prenomClient;
+
+--R82
+
+SELECT nomService
+FROM Services s
+JOIN Proposer p ON p.idService = s.idService
+JOIN Bungalows b ON b.idBungalow = p.idBungalow
+JOIN Campings c ON c.idCamping = b.idCamping
+WHERE nomCamping = 'Les Flots Bleus'
+AND superficieBungalow = (
+	SELECT MAX(superficieBungalow)
+	FROM Bungalows b 
+	JOIN Campings c ON b.idCamping = c.idCamping
+	WHERE nomCamping = 'Les Flots Bleus');
+
+--R83 BAH YAPADEWHERE	
+
+SELECT nomBungalow, COUNT(idLocation) AS nbLoc
+FROM Bungalows b
+LEFT JOIN Locations l ON b.idBungalow = l.idBungalow
+AND dateDebut<='30-06-2017'
+AND dateFin >= '01-06-2017'
+GROUP BY b.idBungalow, nomBungalow
+ORDER BY nbLoc DESC;
+
+--R84
+
+SELECT villeCamping
+FROM Campings
+GROUP BY villeCamping
+HAVING COUNT(*)>1
+
+
+--R85
+SELECT nomClient
+FROM Clients c
+JOIN Locations l ON c.idClient = l.idClient
+GROUP BY c.idClient, nomClient
+HAVING COUNT(idLocation) = (
+	SELECT COUNT(idLocation)
+	FROM Clients c
+	JOIN Locations l ON c.idClient = l.idClient
+	WHERE nomClient = 'Zeblouse'
+	GROUP BY c.idClient, nomClient);
+
+--R86
+
+SELECT nomService
+FROM Services s
+JOIN Proposer p ON s.idService = p.idService
+GROUP BY s.idService, nomService
+HAVING COUNT(idBungalow) < 5;
+
+--R87
+SELECT nomCamping
+FROM Campings c
+JOIN Employes e ON c.idCamping = e.idCamping
+GROUP BY c.idCamping, nomCamping
+HAVING COUNT(idEmploye) = (SELECT MAX(COUNT(idEmploye))
+	FROM  Employes
+	GROUP BY idCamping);
+
+--R88
+SELECT nomBungalow
+FROM Bungalows b
+JOIN Proposer p ON b.idBungalow = p.idBungalow
+JOIN Services s ON s.idService = p.idService
+GROUP BY b.idBungalow, nomBungalow
+HAVING COUNT( DISTINCT categorieService) = (SELECT COUNT(DISTINCT categorieService) FROM Services);
+
+--R89
+SELECT nomBungalow
+FROM Bungalows b
+JOIN Proposer p ON p.idBungalow = b.idBungalow
+WHERE idService IN(
+	SELECT idService
+	FROM Bungalows b
+	JOIN Proposer p ON b.idBungalow = p.idBungalow)
+AND idService NOT IN(
+	SELECT idService
+	FROM Bungalows b
+	JOIN Proposer p ON p.idBungalow = b.idBungalow
+	MINUS
+	SELECT idService
+	FROM Bungalows b
+	JOIN Proposer p ON b.idBungalow = p.idBungalow);
